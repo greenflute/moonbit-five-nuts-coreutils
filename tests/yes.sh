d6a851c172bf5aebfb095278759b2b9cb9012dbe
Meng Zhuo <mengzhuo@iscas.ac.cn>
2024-11-14 23:49:39 +0800

    ci: fix yes raise SIGPIPE

--- tests/yes.sh
+++ tests/yes.sh
@@ -4,4 +4,4 @@ set -e
 # Due to https://github.com/moonbitlang/moon/issues/472
 # This is test using `yes` instead of `moon run src/yes`
 
-yes | head -n 2 | grep -q 'y$'s
+(yes ||:) | head -n 2 | grep -q 'y$'
